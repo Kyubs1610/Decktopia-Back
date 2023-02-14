82309ef7bb775e362f5aec5695809130e4c3cb21
matt485 <113977874+Matt485@users.noreply.github.com>
2023-02-14 15:19:22 +0100

    Manually merge Estime branch

--- backend/controllers/login.mjs
+++ backend/controllers/login.mjs
@@ -18,24 +18,51 @@ export const login = async (req, res) => {
       return res.status(500).send({ message: "An error occurred while trying to log in" });
     }
   };
-  
-
         // const validPassword = await bcrypt.compare(password, user.rows[0].password);
         // if (!validPassword) {
         //     return res.status(401).send({ message: "Email or password is incorrect" });
         // }
         // const token = jwtGenerator(user.rows[0].id);
         // res.json({ token });
-    
 
+// get one user
+export const oneUser = async (req, res) => {
+  const { id } = req.params
+  try {
+    const user = await Pool.query("SELECT * FROM users WHERE id = $1", 
+    [id])
+  } catch (error) {
+    console.error(error.message)
+  }
+}
 // get all users
 export const getUsers = async (req, res) => {
-    try {
-        const allUsers = await Pool.query("SELECT * FROM users");
-        res.json(allUsers.rows);
-    } catch (error) {
-        console.error(error.message);
-    }
+  try {
+      const allUsers = await Pool.query("SELECT * FROM users");
+      res.json(allUsers.rows);
+  } catch (error) {
+      console.error(error.message);
+  }
 }
+
+//logout
+export const logout = async ( req, res ) =>{
+  const { id } = req.params
+  try {
+    const logout = await Pool.query(
+      "SELECT * FROM users where id = $1",
+      [id]
+    )
+    if (logout.rows.length === 0) {
+      return res.status (404)
+      .send({ message: "succesfully logged out " })
+    }
+    } catch (error) {
+      console.error(error.message)
+      res.status(500).send({ message: "An error occurred while trying to log out" }
+    )}
+  }
+
+
 
 
--- backend/controllers/register.mjs
+++ backend/controllers/register.mjs
@@ -11,8 +11,8 @@ export const register = async (req, res) => {
     try {
         // const hashedPassword = await bcrypt.hash(password, 10);
         await Pool.query(
-            "INSERT INTO users ( username, email, password) VALUES ($1, $2, $3) RETURNING *",
-            [ username, email, password]
+            "INSERT INTO users (  username, email, password) VALUES ($1, $2, $3) RETURNING *",
+            [  username, email, password]
         );
         res.status(200).send({ message: "User created successfully"});
     } catch (error) {
@@ -27,5 +27,20 @@ export const deleteUser = async (req, res) => {
         res.json("User was deleted!");
     } catch (error) {
         console.error(error.message);
+    }
+}
+
+// forgot password
+export const forgotPassword = async ( req, rest ) =>{
+    try {
+        const { id } = req.params
+        const { password } = req.body
+        const update = await Pool.query(
+        "UPDATE users SET password = $1 WHERE id = $2"
+        [password, id]
+        )
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send({ message: "An error occurred while trying to update the password" });
     }
 }
