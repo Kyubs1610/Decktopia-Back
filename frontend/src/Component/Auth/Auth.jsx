84d01a10ec2e919b5057d516bf363b63e4be60a0
Kyubs1610 <diego.decort@gmail.com>
2023-02-20 11:35:23 +0100

    fetch

--- frontend/src/Component/Auth/Auth.jsx
+++ frontend/src/Component/Auth/Auth.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import "./Auth.css"
 
-fetch('http://localhost:8127/login', {
+fetch('https://decktopia.netlify.app/login', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
