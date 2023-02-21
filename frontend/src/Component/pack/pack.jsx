24018ae1b7934f001561fae0fbb3a040ac666159
Kyubs1610 <diego.decort@gmail.com>
2023-02-21 16:09:50 +0100

    card

--- frontend/src/Component/pack/pack.jsx
+++ frontend/src/Component/pack/pack.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./pack.css";
 import Header from "..//Header_and_footer/header";
 
+
 export default function Pack() {
     return (
         <>
@@ -10,7 +11,7 @@ export default function Pack() {
                 
                     <h3>Buy a pack</h3>
 
-                    <a><img className="packes" src="../..//..//src/Asset/card_and_pack/back.png"></img></a>
+                    <a><img className="packes" alt="cards" src="../..//..//src/Asset/card_and_pack/back.png" /></a>
 
                     <a href="/collection"><button>see collection</button></a>
                 </div>
