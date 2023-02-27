a306195aea35ac48f7a2f1411aa882622a3557c4
NicolasEngels <nicolasengels17@gmail.com>
2023-02-27 11:29:20 +0100

    refacto pack.jsx

--- frontend/src/Component/pack/pack.jsx
+++ frontend/src/Component/pack/pack.jsx
@@ -5,121 +5,107 @@ import generateRandomInteger from "./random"
 import { useState } from "react";
 
 export default function CardFlip() {
-                  const [flippedCards, setFlippedCards] = useState([]);
-                  const [packOpened, setPackOpened] = useState(false);
+    const [flippedCards, setFlippedCards] = useState([]);
+    const [packOpened, setPackOpened] = useState(false);
                 
-                  const flip = (event, index) => {
-                    event.preventDefault();
-                    const cards = document.getElementsByClassName("carta");
-                    const card = cards[index];
-                    if (card.style.transform === "rotateY(180deg)") {
-                      card.style.transform = "rotateY(0deg)";
-                    } else {
-                      card.style.transform = "rotateY(180deg)";
-                    }
-                  }
+    const flip = (event, index) => {
+        event.preventDefault();
+        const cards = document.getElementsByClassName("carta");
+        const card = cards[index];
+        if (card.style.transform === "rotateY(180deg)") {
+            card.style.transform = "rotateY(0deg)";
+        } else {
+            card.style.transform = "rotateY(180deg)";
+        }
+    }
                 
-                  const togglePack = () => {
-                    setPackOpened(!packOpened);
-                  };
+    const togglePack = () => {
+        setPackOpened(!packOpened);
+    };
                 
-                  return (
-                    <>
-                    <div>
-                      
-                      <div
-                        id="pack-opened"
-                        className={`pack-content ${packOpened ? "open" : ""}`}
-                      >
-        <div
-          className="pack-content"
-          style={{ display: "block", visibility: "visible" }}
-        >
-          <div className="pack-flash">
-            <div className="pack-flash-pack">
-              <img
-                className="front"
-                src="https://cdn.discordapp.com/attachments/989739808286974002/1077894624015810590/back.png"
-              />
-              <div className="top">
-                <img src="https://i.imgur.com/b1qmOW6.png" />
-                <div className="cut">
-                  <img src="https://i.imgur.com/k55nnYY.png" />
-                </div>
-                <span>
-                  <img src="https://i.imgur.com/JqedAsJ.png" />
-                  <span>
-                    <img src="https://i.imgur.com/WWRXjri.png" />
-                    <span>
-                      <img
-                        src="https://i.imgur.com/DzEYvSP.png"
-                        style={{ width: "81px" }}
-                      />
-                    </span>
-                  </span>
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className="cards-reveal">
-            <ul>
-              <li className="card-01">
-                <div className="carta" onClick={(e) => flip(e, 0)}>
-                  <div
-                    className={`card charizard animated ${
-                      flippedCards.includes(0) ? "flipped" : ""
-                    }`}
-                    style={{
-                      width: "278px",
-                      height: "409px",
-                      marginTop: "0px",
-                    }}
-                  ></div>
+    return (
+        <>
+            <div>
+                <div id="pack-opened" className={`pack-content ${packOpened ? "open" : ""}`}>
+                    <div className="pack-content" style={{ display: "block", visibility: "visible" }}>
+                        <div className="pack-flash">
+                            <div className="pack-flash-pack">
+                                <img className="front" src="https://cdn.discordapp.com/attachments/989739808286974002/1077894624015810590/back.png"/>
+                                
+                                <div className="top">
+                                    <img src="https://i.imgur.com/b1qmOW6.png" />
+                                    
+                                    <div className="cut">
+                                        <img src="https://i.imgur.com/k55nnYY.png" />
+                                    </div>
+
+                                    <span>
+                                        <img src="https://i.imgur.com/JqedAsJ.png" />
+
+                                        <span>
+                                            <img src="https://i.imgur.com/WWRXjri.png" />
+                                            
+                                            <span>
+                                                <img src="https://i.imgur.com/DzEYvSP.png" style={{ width: "81px" }}/>
+                                            </span>
+                                        </span>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        
+                         <div className="cards-reveal">
+                            <ul>
+                                <li className="card-01">
+                                    <div className="carta" onClick={(e) => flip(e, 0)}>
+                                        <div className={`card charizard animated ${flippedCards.includes(0) ? "flipped" : ""}`}style={{width: "278px",height: "409px",marginTop: "0px",}}></div>
+                                    </div>
+                                </li>
+
+                                <li className="card-02 gold">
+                                    <div className="carta" onClick={(e) => flip(e, 1)}>
+                                        <div className="frente">
+                                            <img src="https://images.pokemoncard.io/images/swsh6/swsh6-157_hires.png" />
+                                        </div>
+                                        
+                                        <div className="tras">
+                                            <img src="https://cdn.discordapp.com/attachments/989739808286974002/1077894624015810590/back.png" />
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <li className="card-03 silver">
+                                    <div className="carta" onClick={(e) => flip(e, 2)}>
+                                        <div className="frente">
+                                            <img src="https://images.pokemoncard.io/images/swsh6/swsh6-157_hires.png" />
+                                        </div>
                   
-                </div>
-              </li>
-
-              <li className="card-02 gold">
-                <div className="carta" onClick={(e) => flip(e, 1)}>
-                  <div className="frente">
-                    <img src="https://images.pokemoncard.io/images/swsh6/swsh6-157_hires.png" />
-                  </div>
-                  <div className="tras">
-                    <img src="https://cdn.discordapp.com/attachments/989739808286974002/1077894624015810590/back.png" />
-                  </div>
-                </div>
-              </li>
-
-              <li className="card-03 silver">
-                <div className="carta" onClick={(e) => flip(e, 2)}>
-                  <div className="frente">
-                    <img src="https://images.pokemoncard.io/images/swsh6/swsh6-157_hires.png" />
-                  </div>
-                  <div className="tras">
-                    <img src="https://cdn.discordapp.com/attachments/989739808286974002/1077894624015810590/back.png" />
-                  </div>
-                </div>
-              </li>
-              <li className="card-04 bronze">
-                <div className="carta" onClick={(e) => flip(e, 3)}>
-                  <div className="frente">
-                    <img src="https://images.pokemoncard.io/images/swsh6/swsh6-157_hires.png" />
-                  </div>
-                  <div className="tras">
-                    <img src="https://cdn.discordapp.com/attachments/989739808286974002/1077894624015810590/back.png" />
-                  </div>
-                </div>
-              </li>
+                                        <div className="tras">
+                                            <img src="https://cdn.discordapp.com/attachments/989739808286974002/1077894624015810590/back.png" />
+                                        </div>
+                                    </div>
+                                </li>
+              
+                                <li className="card-04 bronze">
+                                    <div className="carta" onClick={(e) => flip(e, 3)}>
+                                        <div className="frente">
+                                            <img src="https://images.pokemoncard.io/images/swsh6/swsh6-157_hires.png" />
+                                        </div>
+                 
+                                        <div className="tras">
+                                            <img src="https://cdn.discordapp.com/attachments/989739808286974002/1077894624015810590/back.png" />
+                                        </div>
+                                    </div>
+                                </li>
               
 
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
   
-<button className="btn-open" onClick={togglePack}>
-                        Open This Pack
-                      </button>
-                      </>
-  )};  
+            <button className="btn-open" onClick={togglePack}>Open This Pack</button>
+        </>
+    )
+};  
