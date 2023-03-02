c104ce17c682f8f6d7a079dd18648bd699768f10
Estime10 <estimevangu.pro@gmail.com>
2023-03-02 16:16:07 +0100

    table altered

--- database/init.sql
+++ database/init.sql
@@ -11,6 +11,10 @@ CREATE TABLE cards (cards_id SERIAL PRIMARY KEY,
  card_exp VARCHAR(255));
  ALTER TABLE cards 
  DROP COLUMN user_id;
+ ALTER TABLE cards
+ DROP COLUMN card_exp;
+ALTER TABLE cards
+ADD COLUMN card_path VARCHAR(255);
 CREATE TABLE collection_per_user (collection_id SERIAL PRIMARY KEY,
 user_id INTEGER REFERENCES users(id),
 collection_name VARCHAR(255),
