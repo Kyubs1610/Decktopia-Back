55b4b88544f65bd6182b921f12605d2a834313b0
matt485 <113977874+Matt485@users.noreply.github.com>
2023-03-06 07:29:22 +0100

    Adapt backend login auth method

--- backend/controllers/login.mjs
+++ backend/controllers/login.mjs
@@ -40,16 +40,18 @@ export const login = async (req, res) => {
 
     // If the passwords match, generate a JWT token and send it in a cookie
     if (match) {
-      const token = await sign({ email: result.email }, process.env.JWT, {
-        expiresIn: "1h",
-        algorithm: "HS256",
-      });
-      console.log(token)
-      res.cookie("access_token", token, {
-        httpOnly: true,
-      });
+      // const token = await sign({ email: result.email }, process.env.JWT, {
+      //   expiresIn: "1h",
+      //   algorithm: "HS256",
+      // });
+      // console.log(token)
+      // res.cookie("access_token", token, {
+      //   httpOnly: true,
+      // });
 
-      res.send({ message: "Login successful" });
+      res
+        .cookie('user_name', result.username, { expires: 0, sameSite: "Lax"}) // TODO username auth method
+        .send({ message: "Login successful" });
       
     } else {
       // If the passwords don't match, return 401 error
@@ -63,25 +65,25 @@ export const login = async (req, res) => {
 
 
 
-// get one user
-export const oneUser = async (req, res) => {
-  const { id } = req.params
-  try {
-  await Pool.query("SELECT * FROM users WHERE id = $1", 
-    [id])
-  } catch (error) {
-    console.error(error.message)
-  }
-}
-// get all users
-export const getUsers = async (req, res) => {
-  try {
-      const allUsers = await Pool.query("SELECT * FROM users");
-      res.json(allUsers.rows);
-  } catch (error) {
-      console.error(error.message);
-  }
-}
+// // get one user
+// export const oneUser = async (req, res) => {
+//   const { id } = req.params
+//   try {
+//   await Pool.query("SELECT * FROM users WHERE id = $1", 
+//     [id])
+//   } catch (error) {
+//     console.error(error.message)
+//   }
+// }
+// // get all users
+// export const getUsers = async (req, res) => {
+//   try {
+//       const allUsers = await Pool.query("SELECT * FROM users");
+//       res.json(allUsers.rows);
+//   } catch (error) {
+//       console.error(error.message);
+//   }
+// }
 
 //logout
 export const logout = async ( req, res ) =>{
