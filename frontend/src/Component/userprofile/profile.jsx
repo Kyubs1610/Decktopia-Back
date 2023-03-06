be63135b811c4af3b721354488200a5a147e0335
matt485 <113977874+Matt485@users.noreply.github.com>
2023-03-06 07:39:13 +0100

    Add collection to profile

--- frontend/src/Component/userprofile/profile.jsx
+++ frontend/src/Component/userprofile/profile.jsx
@@ -2,7 +2,7 @@ import "./profile.css";
 import "..//..//App.css";
 import React, { useState, useRef, useEffect } from "react";
 import Header from "..//Header_and_footer/header";
-import image from "..//..//..//src/Asset/card_and_pack/back_empty.png";
+// import image from "..//..//..//src/Asset/card_and_pack/back_empty.png";
 
 import Cookies from 'js-cookie'
 
@@ -27,25 +27,45 @@ const getProfileData = () => {
 
 function ProfilePage() {
 
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
   console.log("USERNAME: " + getUserName())
+
+  // for user data
+  const [profileData, setProfileData] = useState(null);
+  const [profileLoading, setProfileLoading] = useState(true);
+  const [profileError, setProfileError] = useState(null);
 
   useEffect(() => {
     fetch("http://51.38.32.47:8000/user/" + getUserName())
       .then(response => response.json())
       .then((userData) => {
         console.log(userData);
-        setLoading(false);
-        setData(userData);
+        setProfileLoading(false);
+        setProfileData(userData);
       })
       .catch((e) => {
         console.error(`An error occurred: ${e}`)
       });
   }, []);
 
+  // for collection data
+  const [collectionData, setCollectionData] = useState(null);
+  const [collectionLoading, setCollectionLoading] = useState(true);
+  const [collectionError, setCollectionError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://51.38.32.47:8000/collection/" + 1)
+      .then(response => response.json())
+      .then((collData) => {
+        console.log(collData);
+        setCollectionLoading(false);
+        setCollectionData(collData);
+      })
+      .catch((e) => {
+        console.error(`An error occurred: ${e}`)
+      });
+  }, []);
+
+  // Avatar
   const [avatar, setAvatar] = useState(null);
   const fileInputRef = useRef();
 
@@ -95,6 +115,7 @@ function ProfilePage() {
         <h3 className="justifyCenter"> Best Deck </h3> 
        
         <div className="collection-container">
+            {collectionData ? "YES" : "NO"}
           <img src={image} className="collection-card" alt="empty card"/>
           <img src={image} className="collection-card" alt="empty card"/>
           <img src={image} className="collection-card" alt="empty card"/>
